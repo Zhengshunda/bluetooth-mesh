949df8b948d4a19832ef0068d052503c81b36595
Zhengshunda <daishunda@outlook.com>
2024-03-05 16:03:58 +0800

    删除了没用的几个文件

--- mesh_1DCNN/build_1/image_preload.cmake
+++
@@ -1,8 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "d:/bluemesh/tflite/tflite-v2/mesh_1dcnn;d:/bluemesh/tflite/tflite-v2/mesh_1;d:/bluemesh/mesh;d:/bluemesh/tflite/tflite-v2/mesh_2.4.2;D:/ncs/v2.4.2/v2.4.2/nrf;D:/ncs/v2.4.2/v2.4.2/zephyr" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "D:/bluemesh/tflite/tflite-v2/mesh_1DCNN/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "D:/bluemesh/tflite/tflite-v2/mesh_1DCNN/boards/nrf52840dk_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:/ncs/toolchains/31f4403e35/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
