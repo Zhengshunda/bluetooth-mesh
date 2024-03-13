53f629b3dc0d59c312dd54f32ca02dd0c6138402
Zhengshunda <daishunda@outlook.com>
2024-03-13 10:47:56 +0800

    save the workspace

---
+++ mesh/build/image_preload.cmake
@@ -0,0 +1,6 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "D:/bluemesh/tflite/tflite-v2/bluetooth-mesh/mesh/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "D:/bluemesh/tflite/tflite-v2/bluetooth-mesh/mesh/boards/nrf52840dk_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "D:\ncs\toolchains\v2.1.2\opt\bin\python.exe" CACHE INTERNAL "NCS child image controlled")
